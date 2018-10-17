b317bf185aa5a2479cd280dd5c86c8d00fe12b98
Brietta Smith <bri@Briettas-MacBook-Pro.local>
2018-10-16 23:01:02 -0400

    add

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <hr />
+    <p className="pull-right">
+      <i className="foot" aria-hidden="true" /> Designed by BCS
+    </p>
+  </footer>
+);
+
+export default Footer;
